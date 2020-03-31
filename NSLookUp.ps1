7de9e9ad68a914dc6a465486adf4f95d25f8d610
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2020-03-31 09:13:03 +0200

    Create NSLookUp.ps1

---
+++ NSLookUp.ps1
@@ -0,0 +1,14 @@
+$servers = get-content "C:\x\x\x\hostlist.txt"
+foreach ($Server in $Servers)
+{
+    $Addresses = $null
+    try {
+        $Addresses = [System.Net.Dns]::GetHostAddresses("$Server").IPAddressToString
+    }
+    catch { 
+        $Addresses = "Server IP cannot resolve."
+    }
+    foreach($Address in $addresses) {
+        write-host $Server, $Address 
+    }
+}
