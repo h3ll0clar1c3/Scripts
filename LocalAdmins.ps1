c9292a5e8a5232c0150ecf7349ec1494aa0f38cc
h3ll0clar1c3 <61941106+h3ll0clar1c3@users.noreply.github.com>
2020-04-06 14:21:22 +0200

    Create LocalAdmins.ps1
    Checks a hosts file for members of the Administrators, Remote Desktop Users, and Users group.

---
+++ LocalAdmins.ps1
@@ -0,0 +1,17 @@
+$servers = Get-Content C:\servers.txt
+$groups = 'Administrators', 'Remote Desktop Users', 'Users'
+$results = foreach ($server in $servers) {
+    foreach ($group in $groups) {
+        $obj = [ADSI]"WinNT://$server/$group"
+        $obj.Invoke('Members') | % {
+            $path = ([adsi]$_).path
+            [pscustomobject]@{
+                Computer = $server
+                Group = $group
+                Domain = $(Split-Path (Split-Path $path) -Leaf)
+                User = $(Split-Path $path -Leaf)
+            }
+        }
+    }
+}
+$results
